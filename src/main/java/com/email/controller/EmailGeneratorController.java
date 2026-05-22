9aa8f259d12b883ea614d569fc50bb86809c83aa
mohitrhl <mohitrhl001@gmail.com>
2026-05-22 16:07:14 +0530

    update cors

--- src/main/java/com/email/controller/EmailGeneratorController.java
+++ src/main/java/com/email/controller/EmailGeneratorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://email-writer-frontend-ai.vercel.app")
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
